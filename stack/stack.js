eaaceafb4745a3741b8f0e7635254854c06305d3
Nick Bartlett <nickbartlett19@gmail.com>
2023-12-05 13:32:35 -0500

    initial commit

---
+++ stack/stack.js
@@ -0,0 +1,22 @@
+class Stack {
+    constructor(size) {
+        this.size = size;
+    }
+
+    push() {
+
+    }
+
+    pop() {
+
+    }
+
+    // peek() {
+
+    // }
+}
+
+
+const myStack = new Stack(4);
+
+console.log(myStack);
